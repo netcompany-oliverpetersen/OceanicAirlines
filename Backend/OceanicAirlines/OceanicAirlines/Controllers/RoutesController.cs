c6de01bf4fbd07e3996b5c619799c4d70024ffc6
Johan Møller Jakobsen <jmja@netcompany.com>
2022-05-05 13:01:39 +0200

    New Controller

---
+++ Backend/OceanicAirlines/OceanicAirlines/Controllers/RoutesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OceanicAirlines.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutesController : ControllerBase
+    {
+        private readonly ILogger<RoutesController> _logger;
+
+        public RoutesController(ILogger<RoutesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetRoutes")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
