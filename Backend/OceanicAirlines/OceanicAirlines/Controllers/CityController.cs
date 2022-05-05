165e3f5d2eb4106135ac81ce3e2e5e444e9cfb61
Johan Møller Jakobsen <jmja@netcompany.com>
2022-05-06 00:03:13 +0200

    Cities  + Shipping history API

---
+++ Backend/OceanicAirlines/OceanicAirlines/Controllers/CityController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OceanicAirlines.APIModels;
+using OceanicAirlines.Services;
+using OceanicAirlines.Models;
+using System.Collections;
+
+namespace OceanicAirlines.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController
+    {
+        [HttpGet(Name = "GetCities")]
+        public async Task<IEnumerable<City>> Get()
+        {
+            List<City> cities = new List<City>();
+            using (var context = new DbOaDk1Context())
+            {
+                cities = context.City.ToList();
+            }
+            return cities;
+        }
+    }
+}
