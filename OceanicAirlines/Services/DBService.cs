3cfeb9f7bcd394bbfe25c460350492b29d9ed899
Johan Møller Jakobsen <jmja@netcompany.com>
2022-05-05 11:14:11 +0200

    Merge db Service setup

---
+++ OceanicAirlines/Services/DBService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OceanicAirlines.Models;
+
+namespace OceanicAirlines.Services
+{
+    public class DBService : DbContext
+    {
+        public DBService(DbContextOptions<DBService> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<City> CityItems { get; set; } = null!;
+    }
+}
