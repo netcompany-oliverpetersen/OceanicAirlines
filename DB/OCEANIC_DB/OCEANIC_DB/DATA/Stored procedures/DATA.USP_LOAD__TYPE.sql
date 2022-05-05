fe3845fd3b68e88de973fbac7ca0e572244c3c45
Dina Sofia Mortensen <dsm@netcompany.com>
2022-05-05 20:20:53 +0200

    Added procedure for RoutePriceTable and edited parcel type names

--- DB/OCEANIC_DB/OCEANIC_DB/DATA/Stored procedures/DATA.USP_LOAD__TYPE.sql
+++ DB/OCEANIC_DB/OCEANIC_DB/DATA/Stored procedures/DATA.USP_LOAD__TYPE.sql
@@ -8,8 +8,8 @@ INSERT INTO DATA.TYPE (Type, Factor, Active)
 VALUES
 ('Recorded Delivery',1,0),
 ('Weapons',2,1),
-('Live animals',1,0),
-('Cautious parcels',1.75,1),
-('Refrigerated goods',1.1,1),
-('Others',1,1)
+('Livestock',1,0),
+('Cautious',1.75,1),
+('Refrigerated',1.1,1),
+('Other',1,1)
 RETURN 0;
