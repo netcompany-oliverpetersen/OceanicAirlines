d690f85610a15248227d4275f4749715298910fe
nc-guag <guag@netcompany.com>
2022-05-05 16:51:07 +0200

    Add engine components

--- Backend/OceanicAirlines/OceanicAirlines/APIModels/ApiRoute.cs
+++ Backend/OceanicAirlines/OceanicAirlines/APIModels/ApiRoute.cs
@@ -11,6 +11,13 @@
             this.Price = model.Price;
             this.Time = model.Time;
         }
+        public ApiRoute(string Start, string End, int Time, int Price)
+        {
+            this.Source = Start;
+            this.Destination = End;
+            this.Price = Price;
+            this.Time = Time;
+        }
         public string Source { get; set; }
         public string Destination { get; set; }
         public int Time { get; set; }
--- Backend/OceanicAirlines/OceanicAirlines/Controllers/WeatherForecastController.cs
+++ Backend/OceanicAirlines/OceanicAirlines/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using OceanicAirlines.Engine;
 
 namespace OceanicAirlines.Controllers
 {
@@ -6,10 +7,6 @@ namespace OceanicAirlines.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -19,15 +16,12 @@ namespace OceanicAirlines.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public string Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            DataAggregator DataAggregator = new DataAggregator();
+            DataAggregator.DataAggregate();
+
+            return "Hello from the DataAggregator";
         }
     }
 }
